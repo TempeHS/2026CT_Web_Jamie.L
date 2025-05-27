78c11ef145a32b8a944d3ea0324098df93adcaa9
Jamie L <jamie.le7@education.nsw.gov.au>
2025-05-27 23:40:12 +0000

    add index

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Mantis Lords", "The Mantis Lords are 3 sisters you must fight and defeat in the Mantis Village", "Go to boss", "static/images/Card_1.png"),
-        ("Nightmare King Grim", "Leader of the grim troupe, and taken over by the nightmare heart", "Go to boss", )
+        ("Mantis Lords", "The three sisters and leaders of the mantis tribe", "Go to boss", "static/images/Card_1.png"),
+        ("Nightmare King Grim", "Leader of the grim troupe, and taken over by the nightmare heart", "Go to boss" ),
+        ("Sly", "Scammer, Shopkeeper and nail art master", "Go to boss" ),
+        ("Gorb", "THE GREAT MIND GORB", "Go to boss"),
     )
     
     return render_template("index.html", cards=card_data), 200
@@ -16,3 +18,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+``
