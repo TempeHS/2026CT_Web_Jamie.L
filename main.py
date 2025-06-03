61502117632fa01d091d98657ed3aaaa655206d3
Jamie L <jamie.le7@education.nsw.gov.au>
2025-06-03 01:55:39 +0000

    add index

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ def index():
         ("Nightmare King Grim", "Leader of the grim troupe, and taken over by the nightmare heart", "Go to boss" ),
         ("Sly", "Scammer, Shopkeeper and nail art master", "Go to boss" ),
         ("Gorb", "THE GREAT MIND GORB", "Go to boss"),
+        ("Hornet", "Protector of hallownest", "Go to boss"),
     )
     
     return render_template("index.html", cards=card_data), 200
 
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
+
+@app.route('/checklist.html')
+def checklist():
+    return render_template("checklist.html"), 200
+
+@app.route('/bossinfo.html')
+def bossinfo():
+    return render_template("bossinfo.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
-``
